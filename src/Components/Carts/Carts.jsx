e86fb5946de86697ee3ef2c44500da51b9785ba7
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-03-31 12:54:48 +0600

    show message on successful payment

--- src/Components/Carts/Carts.jsx
+++ src/Components/Carts/Carts.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React from "react";
+import { BsCartX } from "react-icons/bs";
+import { toast } from "react-toastify";
 
-const Carts = ({cart, setCart}) => {
-    const totalPrice = cart.reduce((sum,item)=> item.price + sum,0);
-    
-    return (
-        <div className='w-7xl mx-auto space-y-5'>
-            <h2 className='text-2xl font-bold'>Your Cart</h2>
-          {cart.map(c=>   <div key={c.id} className='flex justify-between items-center bg-[#F2F2F2] rounded-2xl p-5'>
-                <div className='flex items-center gap-2'>
-                    <div className='bg-white rounded-full p-1 text-2xl'>
-                    {c.icon}
+const Carts = ({ cart, setCart }) => {
+  const totalPrice = cart.reduce((sum, item) => item.price + sum, 0);
+  const handlePayment = () => {
+    cart.length > 0 && toast.success("Payment successful!");
+    setCart([]);
+  };
+  return (
+    <div className="w-7xl mx-auto space-y-3 bg-white rounded-2xl p-5 shadow mt-10">
+      <h2 className="text-2xl font-bold">Your Cart</h2>
+      {cart.length === 0 ? (
+        <div className="text-center space-y-5">
+          <div className="flex justify-center text-5xl">
+            <BsCartX />
+          </div>
+          <p className="text-2xl ">Your cart is empty. Add some products!</p>
+        </div>
+      ) : (
+        <div className="space-y-5">
+          {cart.map((c) => (
+            <div
+              key={c.id}
+              className="flex justify-between items-center bg-[#F2F2F2] rounded-2xl p-5"
+            >
+              <div className="flex items-center gap-2">
+                <div className="bg-white rounded-full p-1 text-2xl">
+                  {c.icon}
                 </div>
                 <div>
-                    <h4 className='text-lg font-semibold'>{c.name}</h4>
-                    <p className='text-base font-medium text-gray-400'>${c.price}</p>
+                  <h4 className="text-lg font-semibold">{c.name}</h4>
+                  <p className="text-base font-medium text-gray-400">
+                    ${c.price}
+                  </p>
                 </div>
-                </div>
-                <p className='text-base text-[#FF3980] cursor-pointer'>Remove</p>
-            </div>)}
-
-
-            <div className='flex justify-between items-center'>
-                <span>Total</span>
-                <span className='text-2xl font-bold'>${totalPrice}</span>
+              </div>
+              <p className="text-base text-[#FF3980] cursor-pointer">Remove</p>
             </div>
+          ))}
 
+          <div className="flex justify-between items-center">
+            <span>Total</span>
+            <span className="text-2xl font-bold">${totalPrice}</span>
+          </div>
 
-            <button className='btn bg-linear-to-l from-[#4F39F6] to-[#9514FA] text-white w-full rounded-full'>Proceed to Checkout</button>
+          <button
+            onClick={handlePayment}
+            className="btn bg-linear-to-l from-[#4F39F6] to-[#9514FA] text-white w-full rounded-full"
+          >
+            Proceed to Checkout
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Carts;
